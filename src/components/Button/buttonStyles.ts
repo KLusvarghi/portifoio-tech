e165c7d73036e15c8231a1b6076282ecc78b0f65
klusvarghi <71743008+KLusvarghi@users.noreply.github.com>
2024-09-15 14:02:08 -0300

    correção da estilziação do button

--- src/components/Button/buttonStyles.ts
+++ src/components/Button/buttonStyles.ts
@@ -44,7 +44,7 @@ export const PrimaryButton = styled(StyledButton)`
 
 export const SecondaryButton = styled(StyledButton)`
   font-weight: 600;
-  color: ${(props) => props.theme.colors.btnSecondary.font};
+  color: ${(props) => props.theme.colors.btnSecondary.colorHover};
   background: ${(props) => props.theme.colors.btnSecondary.bg};
 
   &:hover {
